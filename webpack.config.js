6a2d2783511421e3956644f58a3f15b9dd5d0fc2
Jesse Conway <jessicaeconway12@gmail.com>
2019-11-07 13:41:47 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+// import css from "file.css";
+
+module.exports = {
+    context: __dirname,
+    entry: "./frontend/entry.jsx",
+    output: {
+        path: path.resolve(__dirname, "app", "assets", "javascripts"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                // test: /\.css$/i,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    query: {
+                        presets: [
+                            "@babel/env",
+                            "@babel/react"
+                        ] //              "style-loader", "css-loader"
+                    }
+                }
+            }
+        ]
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    }
+};
+
+//template file
